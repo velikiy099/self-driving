b58ce95bbc18bb851d3fccb8f3356a19a63b42dd
velikiy099 <10955164+velikiy099@users.noreply.github.com>
2022-10-08 14:41:31 +0900

    add sensor

--- car.js
+++ car.js
@@ -10,11 +10,14 @@ class Car {
         this.maxSpeed = 3;
         this.friction = 0.05;
         this.angle = 0;
+
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update() {
+    update(roadBorders) {
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move() {
@@ -69,5 +72,7 @@ class Car {
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
