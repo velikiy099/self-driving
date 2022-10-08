aad332e52ff3f87a322a2fde0252435eb57f61cc
velikiy099 <10955164+velikiy099@users.noreply.github.com>
2022-10-08 10:24:07 +0900

    move car with key controls

--- car.js
+++ car.js
@@ -5,7 +5,22 @@ class Car {
         this.width = width;
         this.height = height;
 
-        this.controls=new Controls();
+        this.controls = new Controls();
+    }
+
+    update() {
+        if (this.controls.forward) {
+            this.y -= 2
+        }
+        if (this.controls.reverse) {
+            this.y += 2
+        }
+        if (this.controls.left) {
+            this.x -= 2
+        }
+        if (this.controls.right) {
+            this.x += 2
+        }
     }
 
     draw(ctx) {
--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
+animate();
+
+function animate() {
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
